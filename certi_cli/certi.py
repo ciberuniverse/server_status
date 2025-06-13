5f7e7c3f8934b6433dd07d9dcce2ff7f9f23e2f7
Hernan Miranda <210466929+ciberuniverse@users.noreply.github.com>
2025-06-13 18:23:34 -0400

    Update certi.py

--- certi_cli/certi.py
+++ certi_cli/certi.py
@@ -481,6 +481,7 @@ cerTI. !! => { http://127.0.0.1:5000 };
     <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://raw.githubusercontent.com/ciberuniverse/server_status/refs/heads/main/images/logo.ico">
     <title>cerTI - Subida</title>
     {css_}
     </head>
